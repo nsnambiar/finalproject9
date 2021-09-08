460c4e407aeba24eebd7201f181e5bbfcf1408e4
nsnambiar <89901428+nsnambiar@users.noreply.github.com>
2021-09-08 15:48:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def load_user(user_id):
 @app.route('/')
 def start():
     #db.create_all()
-    posts = BlogPost.query.order_by(desc(BlogPost.date)).all()
-    issues = IssueBlogPost.query.order_by(desc(IssueBlogPost.id)).all()
+    posts = BlogPost.query.all()
+    issues = IssueBlogPost.query.all()
     return render_template("index.html", all_post=posts, currentuser=current_user, issue_post=issues)
 
 
