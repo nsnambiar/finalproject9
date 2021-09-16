dbd341bd3853bbdb52f17c834fa58b8f6212e139
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-16 15:11:48 +0530

    first

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField, validators,FileField
 from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
-from wtforms.widgets import TextArea
 
 
 class CreatePostForm(FlaskForm):
